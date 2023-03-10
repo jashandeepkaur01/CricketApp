bc8a4cbeefe4225f52ea1c2a9d0db77260b81899
Aakash Saini <aakash.saini@chicmic.co.in>
2023-03-10 10:28:51 +0530

    home update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../App.css';
+import cricketLogo from '../Assets/Images/cricketLogo.png';
 
 function Navbar() {
   return (
     <div className="navouter">
       <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark fs-5 ">
         <div className="container-fluid">
-          <img className="navimg" alt="logo" src="https://o.remove.bg/downloads/34aa9599-f78d-4a40-8644-29ea90b4c434/cricket-clipart-transparent-cricket-batsman-logo-11563308330ptze4bbvyh-removebg-preview.png" />
+          <img className="navimg" alt="logo" src={cricketLogo} />
           <div className="collapse navbar-collapse"  >
             <ul className="navbar-nav ">
               <li className="nav-item ">
