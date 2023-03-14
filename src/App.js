7dc5c351bd53b3cd3e3ee81ff32d96ddd4f7ba46
parshant-1002 <parshant.sharma@chicmic.co.in>
2023-03-14 15:18:07 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/lib/integration/react";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import './App.css'
 
 import RootRouter from "./Routes/RootRouter";
 import { store, persistor } from "./Redux/Store";
-import './App.css'
-import RootRouter from "Routes/RootRouter";
+
 import loginStore from "Redux/loginStore";
 
 function App() {
