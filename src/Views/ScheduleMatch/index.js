0324408bb183dbd25b9db98c9f3407a482c0ae45
parshant-1002 <parshant.sharma@chicmic.co.in>
2023-03-16 13:04:37 +0530

    creating match component

--- src/Views/ScheduleMatch/index.js
+++ src/Views/ScheduleMatch/index.js
@@ -3,9 +3,18 @@ import { useSelector } from 'react-redux'
 function ScheduleMatch() {
 
   return (
-    <>
-      <h2>Schedule Your Match Here</h2>
-    </>
+
+    <div className='d-flex justify-content-center w-100'>
+
+      <div className='d-flex-column justify-content-center m-5 border border-dark w-75 '>
+        <h4 className='m-2'>Match between Team A and Team B</h4>
+        <div className=''>
+          <h4 className='m-2 mt-4'>Select Data</h4>
+          
+        </div>
+      </div>
+
+    </div>
   )
 }
 
