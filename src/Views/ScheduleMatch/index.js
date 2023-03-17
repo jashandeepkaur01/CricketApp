80cc1fcb37563127f54412961953c3c7d3eb7844
parshant-1002 <parshant.sharma@chicmic.co.in>
2023-03-17 10:52:13 +0530

    dfgd

--- src/Views/ScheduleMatch/index.js
+++ src/Views/ScheduleMatch/index.js
@@ -6,110 +6,80 @@ import { useHistory } from 'react-router-dom';
 import { addMatch } from 'Redux/Actions/loginActions';
 import { STRING_ARRAYS } from 'Shared/Constants';
 
-
 function ScheduleMatch() {
-  const [venue, setVenue] = useState("")
-  const [selectedATeam, setSelectedATeam] = useState([])
-  const [selectedBTeam, setSelectedBTeam] = useState([])
-  const [errorMessage, setErrorMessage] = useState("")
+   const [state,setState]=useState({venue:"",selectedATeam:[],selectedBTeam:[],errorMessage:""})
   const dispatch = useDispatch()
   const navigate = useHistory()
   const [value, onChange] = useState(new Date());
-  const arr = (value || "").toString().split(" ")
-  // const g = { "day": arr[0], "month": arr[1], "date": arr[2], "year": arr[3], "time": arr[4] }
-  const matchData = [{ venue: venue, teamA: selectedATeam, teamB: selectedBTeam, date: arr[2], month: arr[1], day: arr[0], year: arr[3], time: arr[4] }]
-
-
-  const date = new Date();
   const teamData1 = useSelector(state => state.loginReducer.teams)
+  // const g = { "day": arr[0], "month": arr[1], "date": arr[2], "year": arr[3], "time": arr[4] }
+  
+  const arr = (value || "").toString().split(" ")
+  const newArr=arr[4].split(":")
+  const matchData = [{ venue: state.venue, teamA: state.selectedATeam, teamB:  state.selectedBTeam, date: arr[2], month: arr[1], day: arr[0], year: arr[3], hour: newArr[0],minute:newArr[1],second:newArr[2] }]
+  const date = new Date();
+  
 
-  function handleChangeTeamA(e) {
-    setSelectedATeam(e)
-  }
-  function handleChangeTeamB(e) {
-    setSelectedBTeam(e)
-  }
+const monthCheck= STRING_ARRAYS.MONTHS.findIndex(val=>val==matchData[0].month)
+  
+
+// && state.selectedATeam != [] && state.selectedBTeam != [] 
+
+ console.log(date.getMinutes(),matchData[0].minute)
 
   function handleStartMatch() {
-    if (venue != "" && selectedATeam != [] && selectedBTeam != [] && value != null) {
-     if(matchData[0].date>=date.getDate()&&matchData[0].year>=date.getUTCFullYear()){
-      dispatch(addMatch(matchData))
-      navigate.push("/scoreComponent")
-    }
-    else{
-      setErrorMessage("Enter current date")
-    }
+    if (state.venue != "" && value != null) {
+      if (matchData[0].date >= date.getDate() && matchData[0].year >= date.getUTCFullYear()&&monthCheck>=date.getMonth()&&date.getHours()<=matchData[0].hour&&date.getMinutes()<=matchData[0].minute) {
+        dispatch(addMatch(matchData))
+        navigate.push("/scoreComponent")
+      }
+      else {
+        setState({...state,  errorMessage:"Cannot schedule match in past "})
+      }
     }
     else {
-      setErrorMessage("Enter all Fields")
-    }
+      setState({...state,  errorMessage:"Enter all Fields"})
+     }
 
   }
+
+  function table(label, content) {
+    return (
+      <tr>
+        <td>
+          <h4 className=' m-2 '>{label}</h4>
+        </td>
+        <td >
+          <div className="ms-5 ps-5">
+            {content}
+          </div>
+        </td>
+      </tr>
+    )
+  }
+
+
+
 
   return (
 
     <div className='d-flex justify-content-center w-100'>
-
       <div className=' m-5 border border-dark round rounded-4 w-50 '>
-
         <div className="d-flex justify-content-center round rounded-4 rounded-bottom border border-success bg-success text-white">
           <h4 className='m-2 mx-5 px-5 '>Match between Team A and Team B</h4>
         </div>
         <table>
-          <tr>
-            <td>
-              <h4 className=' m-2 '>Select Date and time</h4>
-            </td>
-            <td >
-              <div className="ms-5 ps-5">
-
-                <DateTimePicker onChange={onChange} value={value} />
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <h4 className=' m-2  '>Select Venue</h4>
-            </td>
-            <td>
-              <div className="ms-5 ps-5">
-                <input className="w-75" type="text" value={venue} placeholder="Enter venue" onChange={(e) => { setVenue(e.target.value) }}></input>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <h4 className=' m-2  '>Select Your Team</h4>
-            </td>
-            <td>
-              <div className="ms-5 ps-5">
-                <Select options={teamData1?.map(val => ({ label: val.teamName, value: val.teamName }))} onChange={handleChangeTeamA} name="teamCaptain" value={selectedATeam} />
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <h4 className=' m-2  '>Select Oponend's Team</h4>
-            </td>
-            <td>
-              <div className="ms-5 ps-5">
-                <Select options={teamData1?.map(val => ({ label: val.teamName, value: val.teamName }))} onChange={handleChangeTeamB} name="teamCaptain" value={selectedBTeam} />
-              </div>
-            </td>
-          </tr>
-
-
+          {table("Select Date and time", <DateTimePicker onChange={onChange} value={value} />)}
+          {table("Select Venue", <input className="w-75" type="text" value={state.venue} placeholder="Enter venue" onChange={(e) => { setState({...state,venue:e.target.value}) }}></input>)}
+          {table("Select Your Team", <Select options={teamData1?.map(val => ({ label: val.teamName, value: val.teamName }))} onChange={(e)=> setState({...state,selectedATeam:e})} name="teamCaptain" value={state.selectedATeam} />)}
+          {table("Select Oponend's Team", <Select options={teamData1?.map(val => ({ label: val.teamName, value: val.teamName }))} onChange={(e)=> setState({...state,selectedBTeam:e})} name="teamCaptain" value={state.selectedBTeam} />)}
         </table>
         <div className="d-flex p-2 mt-4 round rounded-4 rounded-top border border-success bg-success">
-          <h4 className="text-warning w-50">{errorMessage}</h4>
+          <h4 className="text-warning w-50">{state.errorMessage}</h4>
           <div className="d-flex justify-content-end w-100 ">
-
             <button className="btn btn-outline-light" onClick={handleStartMatch}>Start Match</button>
           </div>
         </div>
-
-
-
       </div>
     </div>
   )
--- src/Views/ScheduledMatches/index.js
+++ src/Views/ScheduledMatches/index.js
@@ -6,11 +6,14 @@ export default function ScheduledMatches() {
     const matchData=useSelector((state=>state.loginReducer.matches))
    
   return (
-    <div>
-        <h1>
-            <h1>{matchData[0].venue}</h1>
-           
-        </h1>
+    <div className='d-flex-column justify-content-center w-100 pt-5 p-5'>
+
+            <h2>venue:{matchData[0].venue}</h2>
+            <h2>date:{matchData[0].date}/{matchData[0].month}/{matchData[0].year}</h2>
+            <h2>teamA:{matchData[0].teamA.value}</h2>
+            <h2>teamB:{matchData[0].teamB.value}</h2>
+     
+    
     </div>
   )
 }
